3c4de890addd490c556f11d0aebaf6501286b87f
Rafaël L. Bekkema <rafael@rafaelbekkema.nl>
2025-12-05 15:42:05 +0100

    Optimize 5 a bit

--- day05.go
+++ day05.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"iter"
+	"slices"
 	"strconv"
 )
 
@@ -57,32 +58,33 @@ func day5b(input iter.Seq[string]) (total int) {
 	for {
 		overlap := false
 
-	outer:
-		for i, r1 := range ranges {
+		for i := 0; i < len(ranges); i++ {
 			for j := i + 1; j < len(ranges); j++ {
-				r2 := ranges[j]
+				suboverlap, r1, r2 := false, ranges[i], ranges[j]
+
 				// r2 starts before, but ends after start of r1
 				if r2[0] < r1[0] && r2[1] >= r1[0] {
 					ranges[i][0] = r2[0]
-					overlap = true
+					suboverlap = true
 				}
 				// r2 ends after, but starts before end of r1
 				if r2[1] > r1[1] && r2[0] <= r1[1] {
 					ranges[i][1] = r2[1]
-					overlap = true
+					suboverlap = true
 				}
 				// r2 falls completely with r1
 				if r2[0] >= r1[0] && r2[1] <= r1[1] {
-					overlap = true
+					suboverlap = true
 				}
 				// r2 same as r1
 				if r2 == r1 {
-					overlap = true
+					suboverlap = true
 				}
-				if overlap {
+				if suboverlap {
 					// remove r2 from list
-					ranges = append(ranges[:j], ranges[j+1:]...)
-					break outer
+					ranges = slices.Delete(ranges, j, j+1)
+					j--
+					overlap = true
 				}
 			}
 		}
