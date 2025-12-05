d0099363d7678172da83e6abbca2ca1e088d7298
Rafaël L. Bekkema <rafael@rafaelbekkema.nl>
2025-12-05 15:02:19 +0100

    Day 5b

--- day05.go
+++ day05.go
@@ -35,5 +35,66 @@ func day5a(input iter.Seq[string]) (total int) {
 		ranges = append(ranges, [2]int{from, to})
 	}
 
-	return total
+	return
+}
+
+func day5b(input iter.Seq[string]) (total int) {
+	var ranges [][2]int
+	for line := range input {
+		if line == "" {
+			break
+		}
+		var from, to int
+		n, _ := fmt.Sscanf(line, "%d-%d", &from, &to)
+		if n != 2 {
+			panic("Unexpected range input: " + line)
+		}
+
+		ranges = append(ranges, [2]int{from, to})
+	}
+
+	// Merge overlapping ranges until there are none
+	for {
+		overlap := false
+
+	outer:
+		for i, r1 := range ranges {
+			for j := i + 1; j < len(ranges); j++ {
+				r2 := ranges[j]
+				// r2 starts before, but ends after start of r1
+				if r2[0] < r1[0] && r2[1] >= r1[0] {
+					ranges[i][0] = r2[0]
+					overlap = true
+				}
+				// r2 ends after, but starts before end of r1
+				if r2[1] > r1[1] && r2[0] <= r1[1] {
+					ranges[i][1] = r2[1]
+					overlap = true
+				}
+				// r2 falls completely with r1
+				if r2[0] >= r1[0] && r2[1] <= r1[1] {
+					overlap = true
+				}
+				// r2 same as r1
+				if r2 == r1 {
+					overlap = true
+				}
+				if overlap {
+					// remove r2 from list
+					ranges = append(ranges[:j], ranges[j+1:]...)
+					break outer
+				}
+			}
+		}
+
+		if !overlap {
+			break
+		}
+	}
+
+	for _, r := range ranges {
+		total += r[1] - r[0] + 1
+	}
+
+	return
 }
--- day05_test.go
+++ day05_test.go
@@ -5,8 +5,7 @@ import (
 	"testing"
 )
 
-func TestDay5a_Example(t *testing.T) {
-	input := `3-5
+var day5aTestInput = `3-5
 10-14
 16-20
 12-18
@@ -17,8 +16,10 @@ func TestDay5a_Example(t *testing.T) {
 11
 17
 32`
+
+func TestDay5a_Example(t *testing.T) {
 	expect := 3
-	result := day5a(strings.SplitSeq(input, "\n"))
+	result := day5a(strings.SplitSeq(day5aTestInput, "\n"))
 	if result != expect {
 		t.Errorf("expected %d, got %d", expect, result)
 	}
@@ -28,6 +29,38 @@ func TestDay5a(t *testing.T) {
 	input := readlines("./inputs/day05.txt")
 	expect := 862
 	result := day5a(input)
+	if result != expect {
+		t.Errorf("expected %d, got %d", expect, result)
+	}
+}
+
+func TestDay5b_Example(t *testing.T) {
+	tt := []struct {
+		input  string
+		expect int
+	}{
+		{day5aTestInput, 14},
+		{"3-5\n3-5", 3},
+		{"3-5\n3-6", 4},
+		{"7-15\n3-5\n5-8", 13},
+		{"1-4\n2-3", 4},
+		{"1-4\n2-4", 4},
+		{"2-3\n1-4", 4},
+	}
+	for _, tc := range tt {
+		t.Run("", func(t *testing.T) {
+			result := day5b(strings.SplitSeq(tc.input, "\n"))
+			if result != tc.expect {
+				t.Errorf("expected %d, got %d", tc.expect, result)
+			}
+		})
+	}
+}
+
+func TestDay5b(t *testing.T) {
+	input := readlines("./inputs/day05.txt")
+	expect := 357907198933892
+	result := day5b(input)
 	if result != expect {
 		t.Errorf("expected %d, got %d", expect, result)
 	}
