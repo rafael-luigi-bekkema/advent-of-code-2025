e0f96987fbc4e6ba7670f0528da321e9c4a6ec3b
Rafaël L. Bekkema <rafael@rafaelbekkema.nl>
2025-12-07 13:14:46 +0100

    Day 7

--- day07.go
+++ day07.go
@@ -1,41 +1,60 @@
 package main
 
 import (
-	"fmt"
 	"iter"
 )
 
-var zero = struct{}{}
+type empty struct{}
+
+var set = empty{}
 
 func day7a(input iter.Seq[string]) (total int) {
-	first := true
-	beams := map[int]struct{}{}
+	beams := map[int]empty{}
 	for line := range input {
-		bline := []byte(line)
 		for i, chr := range []byte(line) {
 			switch chr {
 			case 'S':
-				beams[i] = zero
+				beams[i] = set
 			case '^':
-				_, ok := beams[i]
-				if ok {
+				if _, ok := beams[i]; ok {
 					total++
 					delete(beams, i)
-					beams[i-1] = zero
-					beams[i+1] = zero
+					beams[i-1] = set
+					beams[i+1] = set
 				}
-			case '.':
 			}
 		}
-		for i := range beams {
-			if first {
-				first = false
-				continue
-			}
-			bline[i] = '|'
-		}
-		fmt.Println(string(bline))
 	}
 
 	return total
+}
+
+func day7b(input iter.Seq[string]) (total int) {
+	var lines [][]byte
+	for line := range input {
+		lines = append(lines, []byte(line))
+	}
+
+	beams := map[int]int{}
+	for i, line := range lines {
+		for i, chr := range line {
+			switch chr {
+			case 'S':
+				beams[i] = 1
+			case '^':
+				if val, ok := beams[i]; ok {
+					delete(beams, i)
+					beams[i-1] += val
+					beams[i+1] += val
+				}
+			}
+		}
+		if i == len(lines)-1 {
+			for _, val := range beams {
+				total += val
+			}
+		}
+	}
+
+	return
 }
